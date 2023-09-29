dd1e007c4dcbbfbae09aed83f07c489368ea62e9
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2023-09-29 20:47:55 +0600

    first commit

---
+++ src/Router/route.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "./../pages/Register/Register";
+import Forgot from "./../pages/Fogot/Fogot";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/forgot",
+        element: <Forgot />,
+      },
+    ],
+  },
+]);
